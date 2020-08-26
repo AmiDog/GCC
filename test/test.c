14cdd609d70a38640cc20cfd4a78fb8c4e8c0a48
Mathias Roslund <mathias.roslund@amidog.se>
2020-08-26 12:25:51 +0200

    docs and tests

--- test/test.c
+++ test/test.c
@@ -4,44 +4,84 @@
 
 #define MAKE_TEST(prefix, operation)                                     \
                                                                          \
-void prefix ## test_2(unsigned short *dst, unsigned short *src)          \
+void prefix ## test_u2(unsigned short *dst, unsigned short *src)         \
 {                                                                        \
   *dst operation *src;                                                   \
 }                                                                        \
                                                                          \
-void prefix ## test_4a(unsigned int *dst, unsigned int *src)             \
+void prefix ## test_u4a(unsigned int *dst, unsigned int *src)            \
 {                                                                        \
   *dst operation *src;                                                   \
 }                                                                        \
                                                                          \
-void prefix ## test_4b(unsigned long *dst, unsigned long *src)           \
+void prefix ## test_u4b(unsigned long *dst, unsigned long *src)          \
 {                                                                        \
   *dst operation *src;                                                   \
 }                                                                        \
                                                                          \
-void prefix ## test_8(unsigned long long *dst, unsigned long long *src)  \
+void prefix ## test_u8(unsigned long long *dst, unsigned long long *src) \
 {                                                                        \
   *dst operation *src;                                                   \
 }                                                                        \
                                                                          \
-void prefix ## test_2i(unsigned short *dst)                              \
+void prefix ## test_u2i(unsigned short *dst)                             \
 {                                                                        \
   *dst operation 0x1234;                                                 \
 }                                                                        \
                                                                          \
-void prefix ## test_4ia(unsigned int *dst)                               \
+void prefix ## test_u4ia(unsigned int *dst)                              \
 {                                                                        \
   *dst operation 0x1234;                                                 \
 }                                                                        \
                                                                          \
-void prefix ## test_4ib(unsigned long *dst)                              \
+void prefix ## test_u4ib(unsigned long *dst)                             \
 {                                                                        \
   *dst operation 0x1234;                                                 \
 }                                                                        \
                                                                          \
-void prefix ## test_8i(unsigned long long *dst)                          \
+void prefix ## test_u8i(unsigned long long *dst)                         \
 {                                                                        \
   *dst operation 0x1234;                                                 \
+}                                                                        \
+                                                                         \
+void prefix ## test_s2(signed short *dst, signed short *src)             \
+{                                                                        \
+  *dst operation *src;                                                   \
+}                                                                        \
+                                                                         \
+void prefix ## test_s4a(signed int *dst, signed int *src)                \
+{                                                                        \
+  *dst operation *src;                                                   \
+}                                                                        \
+                                                                         \
+void prefix ## test_s4b(signed long *dst, signed long *src)              \
+{                                                                        \
+  *dst operation *src;                                                   \
+}                                                                        \
+                                                                         \
+void prefix ## test_s8(signed long long *dst, signed long long *src)     \
+{                                                                        \
+  *dst operation *src;                                                   \
+}                                                                        \
+                                                                         \
+void prefix ## test_s2i(signed short *dst)                               \
+{                                                                        \
+  *dst operation -0x1234;                                                \
+}                                                                        \
+                                                                         \
+void prefix ## test_s4ia(signed int *dst)                                \
+{                                                                        \
+  *dst operation -0x1234;                                                \
+}                                                                        \
+                                                                         \
+void prefix ## test_s4ib(signed long *dst)                               \
+{                                                                        \
+  *dst operation -0x1234;                                                \
+}                                                                        \
+                                                                         \
+void prefix ## test_s8i(signed long long *dst)                           \
+{                                                                        \
+  *dst operation -0x1234;                                                \
 }
 
 MAKE_TEST(add, +=)
@@ -49,3 +89,6 @@ MAKE_TEST(sub, -=)
 MAKE_TEST(and, &=)
 MAKE_TEST(or, |=)
 MAKE_TEST(xor, ^=)
+
+MAKE_TEST(mult, *=)
+MAKE_TEST(div, /=)
--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PATH=/usr/local/halfmips/bin:$PATH
 mkdir -p obj
-halfmips-gcc.exe -S -O3 -fno-delayed-branch test.c -o obj/test.s
-halfmips-gcc.exe -S -O3 -fno-delayed-branch -mhalfmips test.c -o obj/test_half.s
-halfmips-gcc.exe -S -O3 -fno-delayed-branch -mhalfmips -mint16 test.c -o obj/test_16.s
+halfmips-gcc.exe -S -O3 -mno-check-zero-division -fomit-frame-pointer -march=r3000 -G0 -fno-builtin -mno-gpopt -nostdlib -msoft-float -fno-delayed-branch test.c -o obj/test.s
+halfmips-gcc.exe -S -O3 -mno-check-zero-division -fomit-frame-pointer -march=r3000 -G0 -fno-builtin -mno-gpopt -nostdlib -msoft-float -fno-delayed-branch -mhalfmips test.c -o obj/test_half.s
+halfmips-gcc.exe -S -O3 -mno-check-zero-division -fomit-frame-pointer -march=r3000 -G0 -fno-builtin -mno-gpopt -nostdlib -msoft-float -fno-delayed-branch -mhalfmips -mint16 test.c -o obj/test_16.s
